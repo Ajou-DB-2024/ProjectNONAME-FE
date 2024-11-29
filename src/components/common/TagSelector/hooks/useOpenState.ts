671f305f5e47f8c0f4664a511353b4a2cf4bb0fc
waffle <nate.crema@gmail.com>
2024-11-29 13:57:37 +0900

    refactor(TagSelector): custom hook 타입설정형식 오기 수정

--- src/components/common/TagSelector/hooks/useOpenState.ts
+++ src/components/common/TagSelector/hooks/useOpenState.ts
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 type useOpenState = (init?: boolean) => [ boolean, () => void ];
 
-export const useOpenState = (init = false) => {
+export const useOpenState: useOpenState = (init = false) => {
   const [ is_open, setIsOpen ] = useState<boolean>(init);
 
   const onSelectorClick = () => {
     return setIsOpen(p => !p);
   }
 
-  return [ is_open, onSelectorClick ] as ReturnType<useOpenState>;
+  return [ is_open, onSelectorClick ];
 }
