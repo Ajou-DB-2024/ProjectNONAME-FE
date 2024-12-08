2ccbaead436f5fa6caf3890ee3b205c7e91e4fa7
waffle <nate.crema@gmail.com>
2024-12-08 16:48:18 +0900

    feat: 개인프로필 전역 레이아웃 추가

---
+++ src/templates/Personal/GlobalLayout/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PersonalGlobalLayoutWrap = styled.section`
+  width: 100%;
+  height: 100%;
+`;
+
+export const PersonalServiceArea = styled.section`
+  width: 100%;
+  height: calc(100% - 6rem);
+`;
